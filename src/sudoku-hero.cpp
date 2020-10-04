a4f107f405b62258c98c639ddc4e954a6ce78176
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-10-04 15:53:08 +0530

    Add isPresentCol method

--- src/sudoku-hero.cpp
+++ src/sudoku-hero.cpp
@@ -4,3 +4,12 @@ using namespace std;
 
 int grid[N][N];
 
+// Check whether num is present in col or not
+bool isPresentInCol(int col, int num)
+{
+    for (int row = 0; row < N; row++)
+        if (grid[row][col] == num)
+            return true;
+    return false;
+}
+
